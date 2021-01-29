7f760b3d352494d7a14653f0db0c65ce59a1a89a
Manasa-BS22 <Manasa.Somashekar@centurylink.com>
2021-01-29 11:58:37 +0000

    Lab 6

--- lab05.main.js
+++ lab05.main.js
@@ -16,10 +16,6 @@ const ServiceNowConnector = require(path.join(__dirname, '/connector.js'));
  */
 const EventEmitter = require('events').EventEmitter;
 
-// We'll use this regular expression to verify REST API's HTTP response status code.
-const validResponseRegex = /(2\d\d)/;
-
-
 /**
  * The ServiceNowAdapter class.
  *
@@ -88,74 +84,20 @@ class ServiceNowAdapter extends EventEmitter {
   }
 
   /**
- * @memberof ServiceNowAdapter
- * @method healthcheck
- * @summary Check ServiceNow Health
- * @description Verifies external system is available and healthy.
- *   Calls method emitOnline if external system is available.
- *
- * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
- *   that handles the response.
- */
-healthcheck(callback) {
- this.getRecord((result, error) => {
-
-     log.error("ERROR"+error);
-     log.error("RESULT"+result);
-     var callback = function(result, error) {
-  if (error) throw error; // Check for the error and throw if it exists.
-  console.log('got data: '+result); // Otherwise proceed as usual.
-};
-       getRecord(callback) ( this.connector.get((result, error) => callback(result,error))); 
-   /**
-    * For this lab, complete the if else conditional
-    * statements that check if an error exists
-    * or the instance was hibernating. You must write
-    * the blocks for each branch.
-    */
-   if (error) {
-     /**
-      * Write this block.
-      * If an error was returned, we need to emit OFFLINE.
-      * Log the returned error using IAP's global log object
-      * at an error severity. In the log message, record
-      * this.id so an administrator will know which ServiceNow
-      * adapter instance wrote the log message in case more
-      * than one instance is configured.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing the error seen as an argument
-      * for the callback's errorMessage parameter.
-      
-      */
-      
-      console.error('Error present.');
-      callbackError = error;
-      this.emitOffline();
-      log.error(callbackError+"id"+this.id);
-    
-     
-   } else {
-     /**
-      * Write this block.
-      * If no runtime problems were detected, emit ONLINE.
-      * Log an appropriate message using IAP's global log object
-      * at a debug severity.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing this function's result
-      * parameter as an argument for the callback function's
-      * responseData parameter.
-      */
-      this.emitOnline();
-      log.debug("no error found");
-      callbackData = result;
-   }
-  
-
-  
- });
-}
-
-
+   * @memberof ServiceNowAdapter
+   * @method healthcheck
+   * @summary Check ServiceNow Health
+   * @description Verifies external system is available and healthy.
+   *   Calls method emitOnline if external system is available.
+   *
+   * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
+   *   that handles the response.
+   */
+  healthcheck(callback) {
+    // We will build this method in a later lab. For now, it will emulate
+    // a healthy integration by emmitting ONLINE.
+    this.emitOnline();
+  }
 
   /**
    * @memberof ServiceNowAdapter
@@ -194,7 +136,6 @@ healthcheck(callback) {
     this.emit(status, { id: this.id });
   }
 
-
   /**
    * @memberof ServiceNowAdapter
    * @method getRecord
@@ -211,13 +152,7 @@ healthcheck(callback) {
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
-      let getCallOptions = { ...this.options };
-    getCallOptions.method = 'GET';
-    getCallOptions.query = 'sysparm_limit=1';
-    this.connector.sendRequest(getCallOptions, (results, error) => callback(results, error));
   }
-
-
 
   /**
    * @memberof ServiceNowAdapter
@@ -235,9 +170,6 @@ healthcheck(callback) {
      * Note how the object was instantiated in the constructor().
      * post() takes a callback function.
      */
-      let getCallOptions = { ...this.options };
-    getCallOptions.method = 'POST';
-    this.connector.sendRequest(getCallOptions, (results, error) => callback(results, error));
   }
 }
 
